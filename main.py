91287b10c36d8b6ef59d741dc24782f134c236ed
andrew park <ceowis@gmail.com>
2025-04-07 13:27:10 +0900

    변경사항 추가 커밋

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QIcon
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        self.btn1=QPushButton('Message', self) #버튼 추가
+        self.btn1.clicked.connect(self.activateMessage) #버튼 클릭시 activateMessage 메소드 호출
+        
+        vbox = QVBoxLayout() # 수직 레이아웃 추가
+        vbox.addStretch(1) #빈 공간 추가
+        vbox.addWidget(self.btn1) #버튼 추가
+        vbox.addStretch(1) #빈 공간 추가
+        
+        self.setLayout(vbox) # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
+        
         self.setWindowTitle('Simple Calculator')
-        self.resize(300, 400)
+        self.setWindowIcon(QIcon('calculator.png')) #아이콘 설정
+        self.resize(256, 256)
         self.show()
+    def activateMessage(self): # 버튼 클릭시 호출되는 메소드: 메시지 박스 출력
+        QMessageBox.information(self, 'information', 'Button Clicked!') #메시지 박스 출력
+        
 if __name__ == '__main__':
   app = QApplication(sys.argv)
   view = Calculator()
